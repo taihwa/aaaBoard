663f8fbcab1089abac6219851bfee8e833dc2a5a
taihwa <dlxoghk1@nate.com>
2015-07-29 11:58:13 +0900

    board.js  추가

---
+++ routes/board.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('board/list', { title: 'aaBoard' });
+});
+
+router.post('/', function(req,res) {
+
+});
+
+/*router.('/', function(req,res) {
+
+});
+
+router.delete('/', function(req,res) {
+
+});
+*/
+module.exports = router;
